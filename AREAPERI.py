016cbdca2686a6d66595a7498d2b9c1e272534e5
worth_it <85249027+kgurnoor@users.noreply.github.com>
2021-07-24 07:10:36 +0530

    codechef problem area perimeter
    https://www.codechef.com/problems/AREAPERI

---
+++ AREAPERI.py
@@ -0,0 +1,14 @@
+# cook your dish here
+l=int(input())
+b=int(input())
+area=l*b 
+peri=(2*(l+b))
+if area>peri:
+    print("Area")
+    print(area)
+if peri>area:
+    print("Peri")
+    print(peri)
+if peri==area:
+    print("Eq")
+    print(eq)
