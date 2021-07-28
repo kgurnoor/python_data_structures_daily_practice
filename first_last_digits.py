9d88840f238263e96e79ddf066b0f887b804308c
worth_it <85249027+kgurnoor@users.noreply.github.com>
2021-07-28 08:14:09 +0530

    codechef practice problem first_last_digits
    https://www.codechef.com/problems/FLOW004

---
+++ first_last_digits.py
@@ -0,0 +1,10 @@
+# cook your dish here
+t=int(input())
+for ty in range(t):
+    n=int(input())
+    number=str(n)
+    first=int(number[0])
+    last=int(number[-1])
+    sum=first+last
+    print(sum)
+    
