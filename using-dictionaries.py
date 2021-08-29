8f1aad7aeb6414bee5fd73a03cddeaaa4b8af4fd
worth_it <85249027+kgurnoor@users.noreply.github.com>
2021-08-29 13:35:24 +0530

    using dictionaries in python

---
+++ using-dictionaries.py
@@ -0,0 +1,17 @@
+my_dict={}
+grades={'ana':'B','john':'A+','denise':'A+'}
+a=grades['john']
+b=grades['sylvan']='A'
+c='john' in grades
+d='daniel' in grades
+
+g=grades.keys()
+h=grades.values()
+print(grades)
+print(a)
+print(b)
+print(c)
+print(d)
+
+print(g)
+print(h)
