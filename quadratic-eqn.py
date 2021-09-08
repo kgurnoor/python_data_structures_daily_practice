8c5f37307e374b75b31d778f4b6e8b32fb10b1c7
worth_it <85249027+kgurnoor@users.noreply.github.com>
2021-09-08 23:20:03 +0530

    quadratic equation sum and product of roots

---
+++ quadratic-eqn.py
@@ -0,0 +1,8 @@
+a=int(input("Give me the coefficient 'a' of your quadratic equation:"))
+b=int(input("Give me the coefficient 'b' of your quadratic equation:"))
+c=int(input("Give me the coefficient 'c' of your quadratic equation:"))
+sum=(-b/a)
+product=(c/a)
+
+print("sum of roots:" ,sum )
+print("product of roots:", product)
