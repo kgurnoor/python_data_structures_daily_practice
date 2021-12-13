cfd30681bfdb0ee32ccabe7369d8901b8d59b825
worth_it <85249027+kgurnoor@users.noreply.github.com>
2021-12-14 05:19:31 +0530

    Object Oriented Programming Example
    https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-0001-introduction-to-computer-science-and-programming-in-python-fall-2016/lecture-videos/lecture-9-python-classes-and-inheritance/ 
    MIT opencourseware example.

---
+++ object oriented programming examples.py.py
@@ -0,0 +1,90 @@
+import random
+
+#################################
+## Animal abstract data type
+#################################
+class Animal(object):
+    def __init__(self, age):
+        self.age = age
+        self.name = None
+    def get_age(self):
+        return self.age
+    def get_name(self):
+        return self.name
+    def set_age(self, newage):
+        self.age = newage
+    def set_name(self, newname=""):
+        self.name = newname
+    def __str__(self):
+        return "animal:"+str(self.name)+":"+str(self.age)
+
+
+
+
+#################################
+## Inheritance example
+#################################
+class Cat(Animal):
+    def speak(self):
+        print("meow")
+    def __str__(self):
+        return "cat:"+str(self.name)+":"+str(self.age)
+
+
+
+
+#################################
+## Inheritance example
+#################################
+class Person(Animal):
+    def __init__(self, name, age):
+        Animal.__init__(self, age)
+        self.set_name(name)
+        self.friends = []
+    def get_friends(self):
+        return self.friends
+    def speak(self):
+        print("hello")
+    def add_friend(self, fname):
+        if fname not in self.friends:
+            self.friends.append(fname)
+    def age_diff(self, other):
+        diff = self.age - other.age
+        print(abs(diff), "year difference")
+    def __str__(self):
+        return "person:"+str(self.name)+":"+str(self.age)
+
+
+
+
+#################################
+## Inheritance example
+#################################
+class Student(Person):
+    def __init__(self, name, age, major=None):
+        Person.__init__(self, name, age)
+        self.major = major
+    def __str__(self):
+        return "student:"+str(self.name)+":"+str(self.age)+":"+str(self.major)
+    def change_major(self, major):
+        self.major = major
+    def speak(self):
+        r = random.random()
+        if r < 0.25:
+            print("i have homework")
+        elif 0.25 <= r < 0.5:
+            print("i need sleep")
+        elif 0.5 <= r < 0.75:
+            print("i should eat")
+        else:
+            print("i am watching tv")
+
+print("\n---- student tests ----")
+s1 = Student('alice', 20, "CS")
+s2 = Student('beth', 18)
+print(s1)
+print(s2)
+print(s1.get_name(),"says:", end=" ")
+s1.speak()
+print(s2.get_name(),"says:", end=" ")
+s2.speak()
