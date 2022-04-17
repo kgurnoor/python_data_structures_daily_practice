d6b3ba2e0e030569e3d12678849e8201556ed84c
worth_it <85249027+kgurnoor@users.noreply.github.com>
2022-04-17 15:04:19 +0530

    https://leetcode.com/problems/multiply-strings/
    This is a function to multiply strings from Leetcode

---
+++ multiply_strings.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        x = int(num1)
+        y = int(num2)
+        product = x*y
+        ans = str(product)
+        return ans
