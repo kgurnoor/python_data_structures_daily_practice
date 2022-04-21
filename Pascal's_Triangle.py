22cfe125a75cef2061f8b1cb87918679afb81ba5
worth_it <85249027+kgurnoor@users.noreply.github.com>
2022-04-21 16:19:14 +0530

    https://leetcode.com/problems/pascals-triangle/submissions/
    Leetcode Problem Solution - Pascal's Triangle : Data Structures

---
+++ Pascal's_Triangle.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+         result=[]
+        for i in range(numRows):
+            if(i ==0):
+                row = [1]
+                result.append(row)
+
+            else:
+                row1 = [1]
+                j = 1
+                while(j < i):
+                    row1.append(row[j-1] + row[j])
+                    j+=1
+                    print j
+                row1.append(1)
+                result.append(row1)
+                row = row1
+        return result
