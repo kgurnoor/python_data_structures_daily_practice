c918d4d1edf17522ea84acb09aa5773335cf060f
worth_it <85249027+kgurnoor@users.noreply.github.com>
2022-04-27 17:04:20 +0530

    https://leetcode.com/problems/valid-parentheses/
    Leetcode Problem Solution : Valid Parentheses : Data Structures

---
+++ Valid_Parentheses.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        leftSymbols = []
+        for i in s:
+            if i in ['(', '{', '[']:
+                leftSymbols.append(i)
+
+            elif i == ')' and len(leftSymbols) != 0 and leftSymbols[-1] == '(':
+
+                leftSymbols.pop()
+
+            elif i == '}' and len(leftSymbols) != 0 and leftSymbols[-1] == '{':
+                leftSymbols.pop()
+            elif i == ']' and len(leftSymbols) != 0 and leftSymbols[-1] == '[':
+                leftSymbols.pop()
+
+            else:
+                return False
+
+        return leftSymbols == []
+        
